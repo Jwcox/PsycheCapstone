b900867514a8d0ebfe1fcad791a92d939d17f594
SethATurnage <turnageseth@gmail.com>
2019-02-25 20:33:39 -0700

    Add project files.

---
+++ Psyche.App/Psyche.App/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Psyche.App.Models;
+
+namespace Psyche.App.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
